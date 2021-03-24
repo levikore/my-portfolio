afaded7f8718dccf54654ed0ab8fba67d435ed74
levikore <41550958+levikore@users.noreply.github.com>
2021-03-24 05:56:05 +0200

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar"
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-   <Navbar/>
+    <>
+      <Particles
+      className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape:{
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header />
+    </>
   );
 }
 
