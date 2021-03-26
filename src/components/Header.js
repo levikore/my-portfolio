70c9c17e8c83c8df13a1b2c02c97dbf1184a4a93
levikore <41550958+levikore@users.noreply.github.com>
2021-03-26 03:48:15 +0300

    changed typed header in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import React from "react"
 import Typed from "react-typed";
 
+const skills = ["Junior Software Developer", "Junior Full-Stack Developer", "Junior Web Developer"];
 const Header = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
-                <h1>web development and website promotions </h1>
-                <Typed
-                    className="typed-text"
-                    strings={["Web Design", "Web Development", "Facebook Ads"]}
-                    typeSpeed={40}
-                    backSpeed={60}
-                    loop
-                />
-
+                <h1>Levi Korenblit</h1>
+                <p className = "typed-wrap">
+                    Looking for a <Typed
+                        className="typed-text"
+                        strings={skills}
+                        typeSpeed={100}
+                        backSpeed={60}
+                        loop
+                    /> position
+                </p>
                 <a href="#" className="btn-main-offer">Contact Me!</a>
             </div>
         </div>
